103fd3061d4719ebc73e6d503c4bde50c10382d8
Julian Gruber <julian@juliangruber.com>
2012-10-12 14:25:32 +0200

    refactorings

--- server.js
+++ server.js
@@ -1,60 +1,33 @@
-var pushover = require('pushover');
 var http = require('http');
-var wd = require('./lib/wd');
-var spawn = require('child_process').spawn;
-var mkdirp = require('mkdirp');
-var Seq = require('seq');
+var pushover = require('pushover');
+var filed = require('filed');
+var Store = require('./lib/store');
 
-var host = process.env.HOST || 'localhost';
-var port = process.env.PORT || 3000;
-var reposPath = process.env.REPOS || __dirname+'/repos';
-var publicPath = process.env.PUBLIC || __dirname+'/public';
+var store = Store({
+  host : 'localhost',
+  port : 3000,
+  root : __dirname + '/static'
+})
 
-var repos = pushover(reposPath);
-
-function store(opts) {
-  var public = opts.public + '/' + opts.repo + '/' + opts.co;
-  Seq()
-    .seq(function() {
-      var ps = spawn('git', [
-        'clone', 'http://' + host + ':' + port + '/' + opts.repo,
-        public
-      ]);
-      ps.stderr.pipe(process.stderr, { end : false });
-      ps.on('exit', this.ok);
-    })
-    .seq(function() {
-      if (opts.co == 'master') return;
-      var ps = spawn('git', ['checkout', opts.co], { cwd : public });
-      ps.stderr.pipe(process.stderr, { end : false });
-    })
-  ;
-}
+var repos = pushover(__dirname + '/repos');
 
 repos.on('push', function(push) {
-  store({
-    repo : push.repo,
-    public : publicPath,
-    co : push.branch
-  });
-
   push.accept();
+  store(push.repo, push.branch);
 });
 
 repos.on('tag', function(tag) {
-  store({
-    repo : tag.repo,
-    public : publicPath,
-    co : tag.version
-  })
-
   tag.accept();
+  store(tag.repo, tag.version);
 })
 
-var server = http.createServer(function(req, res) {
+http.createServer(function(req, res) {
   repos.handle(req, res);
-});
+}).listen(3000);
 
-server.listen(port, function() {
-  console.log('Server listening on port', port);
-});
+http.createServer(function(req, res) {
+  req
+    .pipe(filed(__dirname + '/static' + req.url))
+    .pipe(res)
+  ;
+}).listen(8000);
