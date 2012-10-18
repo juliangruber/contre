972414d2aeafc579a1259ff631740730f7b159cb
Julian Gruber <julian@juliangruber.com>
2012-10-18 12:25:28 +0200

    old node compat

--- bin/contre.js
+++ bin/contre.js
@@ -3,6 +3,7 @@
 var Contre = require('..');
 var http = require('http');
 var fs = require('fs');
+var path = require('path');
 
 if (!fs.existsSync('repos')) fs.mkdirSync('repos');
 if (!fs.existsSync('static')) fs.mkdirSync('static');
@@ -15,4 +16,8 @@ var contre = Contre({
 var port = process.argv[2] || 3000;
 http.createServer(contre.handle()).listen(port, function() {
   console.log('contre listening on port ' + port);
-})
+});
+
+var exists = function(file) {
+  return (fs.existsSync || path.existsSync)(file);
+}
